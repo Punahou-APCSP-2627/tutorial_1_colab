f9901efdcad903addf5e065f20e715315ed4b4b8
Samantha Berman <142542396+bermanpunahou@users.noreply.github.com>
2026-09-14 12:16:38 -1000

    Update playground.py with Colab instructions
    Added instructions for running the program in Google Colab.

--- playground.py
+++ playground.py
@@ -1,6 +1,8 @@
 '''
 Use this program to test out the topics in tutorial.md.
 
+For tutorial 1, you'll need to create this inside of Google Colab.
+
 To run from the command line:
 1. Navigate so that you are "inside" the tutorial_1 directory.
 2. Type python playground.py and press Enter.
