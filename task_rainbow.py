65edbfb364f08ec32b56d776c47810849b1f0cd0
Samantha Berman <142542396+bermanpunahou@users.noreply.github.com>
2026-09-14 12:17:05 -1000

    Update docstring with Google Colab instructions
    Added instructions for running the script in Google Colab.

--- task_rainbow.py
+++ task_rainbow.py
@@ -1,6 +1,8 @@
 '''
 Draws a rainbow on a blue background.
 
+For tutorial 1, you'll need to create this inside of Google Colab.
+
 To run from the command line:
 1. Navigate so that you are "inside" the tutorial_1 directory.
 2. Type python task_rainbow.py and press Enter.
